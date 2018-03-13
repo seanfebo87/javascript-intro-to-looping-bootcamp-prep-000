b35152390c6aabe89f3078188f48764442a42492
Sean Febo <seanfebo@techie.com>
2018-03-13 04:19:05 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ function forLoop(array) {
   for(var i = 0; i < 25; i++) {
     return "I am ${i} strange loops."
   }
-  else(var i = 1;) {
+  if(var i = 1;) {
     return "I am 1 strange loop."
   }
 }
